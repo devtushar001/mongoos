c3ec3c5349348cb3769791b1fe931086e4a086be
devtushar001 <tc341269@gmail.com>
2024-09-25 02:06:30 -0500

    sorting methods in mongodb

--- controllers/find.js
+++ controllers/find.js
@@ -3,8 +3,16 @@ import movieModel from "../models/Movies.js";
 const findeMethod = async () => {
     try {
         // const result = await movieModel.find({ name: "Eternals"});
-        const result = await movieModel.find({ name: "Luca"});
-        console.log(result);
+        // const result = await movieModel.find({ name: "Luca"});
+        // const result = await movieModel.find().skip(18);
+        // const result = await movieModel.find().countDocuments();
+        const result = await movieModel.find().sort({name: -1});
+        // const result = await movieModel.find().sort({ratings: -1});
+        result.forEach(movie => {
+            console.log(movie.name);
+            console.log(movie.ratings);
+        })
+        // console.log(result);
     } catch (error) {
         console.log(error.name);
         console.log(error.message);
